ddad6667c785b86f9c49d3facff91a13385cf184
TAOtxi <120470690+TAOtxi@users.noreply.github.com>
2024-07-22 01:16:03 +0800

    🎉Create

---
+++ README.md
@@ -0,0 +1,10 @@
+# 𝑭𝒍𝒊𝒑 𝑪𝒍𝒐𝒄𝒌
+
+Reference from [WidgetStore](https://cn.widgetstore.net/#/detail/6d85a2b962c6cb270f16f0f63568ed50)
+
+At the beginning of learning html, I was inspired by this component, so I tried to make a clock flip myself.
+
+![Preview](preview.gif)
+
+
+
---
+++ index.js
@@ -0,0 +1,66 @@
+function updateNumber(el, time) {
+    el.children[0].className = "front number-" + time;
+
+    if (time === 59)
+        el.children[1].className = "back number-" + 0;
+    else
+        el.children[1].className = "back number-" + (time + 1);
+}
+
+function reductionCard() {
+    second.classList.remove('flipping');
+    minute.classList.remove('flipping');
+    hour.classList.remove('flipping');
+}
+
+function updateTime() {
+    let date = new Date();
+
+    h = date.getHours();
+    m = date.getMinutes();
+    s = date.getSeconds();
+}
+
+function flip() {
+    second.classList.add('flipping');
+    if (s === 0) {
+        minute.classList.add('flipping');
+
+        if (m === 0) {
+            hour.classList.add('flipping');
+        }
+    }
+}
+
+function init() {
+    updateTime();
+    updateNumber(second, s);
+    updateNumber(minute, m);
+    updateNumber(hour, h);
+}
+
+function run() {
+    updateTime();
+    flip();
+
+    setTimeout(() => {
+        reductionCard();
+
+        updateNumber(second, s);
+        if (s === 0) {
+            updateNumber(minute, m);
+            
+            if (m === 0)
+                updateNumber(hour, h);
+        }
+    }, 500);
+}
+
+const hour = document.querySelector("#hour");
+const minute = document.querySelector("#minute");
+const second = document.querySelector("#second");
+
+var h, m, s;
+
+init();
+setInterval(run, 1000);
